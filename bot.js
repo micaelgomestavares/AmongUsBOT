20f5eb012b338a06b234b77fed8b2ebfbe717a6c
Micael <contato.micaeloficial@gmail.com>
2021-02-12 13:51:55 -0300

    refactoring code

--- bot.js
+++ bot.js
@@ -4,8 +4,9 @@ const client = new Discord.Client();
 const { join } = require('path');
 const { readdirSync } = require('fs');
 
-const { prefix, token } = require('./config.json');
-const { commands } = require('./queue.js');
+const { prefix } = require('./config.json');
+
+const { commands } = require('./controllers/queue.js');
 
 const commandFiles = readdirSync(join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 
@@ -14,10 +15,10 @@ for (const file of commandFiles) {
     commands.set(command.name, command);
 }
 
-client.on("ready", () => {
+client.on('ready', () => {
     let activities = [
-        `Tierry e sofrendo por ela`,
-        "Pisero e sofrendo também"
+        'Tierry e sofrendo por ela',
+        'Pisero e sofrendo também'
     ], i = 0;
 
     client.user.setActivity(`${activities[i++ % activities.length]}`, {
@@ -27,7 +28,7 @@ client.on("ready", () => {
     console.log('tô ligado bb');
 });
 
-client.on("message", async message => {
+client.on('message', async message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -39,7 +40,7 @@ client.on("message", async message => {
 
     if (command.args && !args.length) {
         let reply = `Faltou argumentos, ${message.author}!`;
-        if (command.usage) reply += `\nUse dessa forma: \`${prefix}${command.name} ${command.usage}\``;
+        if (command.usage) reply += `\n Use dessa forma: \`${prefix}${command.name} ${command.usage}\``;
         return message.channel.send(reply);
     }
     try {
@@ -50,4 +51,4 @@ client.on("message", async message => {
     }
 });
 
-client.login(token);
+client.login(process.env.TOKEN);
